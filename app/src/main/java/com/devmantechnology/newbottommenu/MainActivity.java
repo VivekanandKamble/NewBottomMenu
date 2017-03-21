9254b0d715df072ffa292d3bf181d83e966b595e
Sony <vivekanandkamblemail@gmail.com>
2017-03-21 16:53:01 +0530

    Commit

---
+++ app/src/main/java/com/devmantechnology/newbottommenu/MainActivity.java
@@ -0,0 +1,29 @@
+package com.devmantechnology.newbottommenu;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId)
+            {
+               Toast.makeText(MainActivity.this,"ID is : "+ tabId, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
